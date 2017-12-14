48d6b3c26e0db30c0e4f6823e4b2465cc54ca8a8
Walt Zimmerman <zimmermanw84@gmail.com>
2017-12-13 16:47:55 -0800

    add `tokenType` as a param to embed options

--- addon/components/powerbi-report.js
+++ addon/components/powerbi-report.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 import layout from '../templates/components/powerbi-report';
-import { TokenType } from 'powerbi-models';
+import { TokenType } from 'npm:powerbi-models';
 
 export default Ember.Component.extend({
   classNames: ['powerbi-frame'],
